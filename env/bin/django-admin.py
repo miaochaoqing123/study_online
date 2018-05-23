609797f993b2d451ca55ebc47843eb044fe7df7c
miao <419666193@qq.com>
2018-05-23 17:54:08 +0800

    add_user_and_course_models

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/miao/Python/python1704-linux/django_project/MxOnline/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ users/models.py
@@ -0,0 +1,49 @@
+from __future__ import unicode_literals
+from datetime import datetime
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=50, verbose_name=u'昵称', default='')
+    birday = models.DateField(verbose_name=u'生日', null=True, blank=True)
+    gender = models.CharField(max_length=5, choices=(('male', u'男'), ('female', '女')), default='female', verbose_name=u'性别')
+    address = models.CharField(max_length=100, default=u'', verbose_name=u'地址')
+    mobile = models.CharField(max_length=11, null=True, blank=True, verbose_name=u'电话')
+    image = models.ImageField(upload_to='image/%Y/%m', default=u'image/default.png')
+
+    class Meta:
+        verbose_name = u'用户信息'
+        verbose_name_plural = verbose_name
+        db_table = 'userprofile'
+
+    def __unicode__(self):
+        return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name=u'验证码')
+    email = models.EmailField(max_length=50, verbose_name=u'邮箱')
+    send_type = models.CharField(max_length=10, choices=(('register', u'注册'), ('forget', u'忘记密码')), verbose_name=u'发送类型')
+    send_time = models.DateTimeField(default=datetime.now, verbose_name=u'发送时间')
+
+    class Meta:
+        verbose_name = u'邮箱验证码'
+        verbose_name_plural = verbose_name
+        db_table = 'email_verify_record'
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u'标题')
+    image = models.ImageField(upload_to='banner/%Y/%m', verbose_name=u'轮播图')
+    url = models.URLField(max_length=200, verbose_name=u'访问地址')
+    index = models.IntegerField(default=100, verbose_name=u'顺序')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'轮播图'
+        verbose_name_plural = verbose_name
+        db_table = 'banner'
+
+
