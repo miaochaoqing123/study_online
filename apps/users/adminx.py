bd37dcd2006da8cfd6df9f3688aaf7f4d2b471df
miao <419666193@qq.com>
2018-05-24 11:02:51 +0800

    后台定制_修改成中文

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import xadmin
+from xadmin import views
 
 from .models import EmailVerifyRecord, Banner
+
+
+class BaseSetting(object):
+    """后台添加主题选项"""
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSetting(object):
+    """修改后台左上角和底部信息"""
+    site_title = '缪氏后台管理系统'  # 左上角和title
+    site_footer = '缪氏教育在线网'  # 底部
+    menu_style = 'accordion'  # 左边列表的伸缩
 
 
 class EmailVerifyRecordAdmin(object):
@@ -25,7 +39,10 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
-
+# 注册主题
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+# 修改后台左上角和底部信息
+xadmin.site.register(views.CommAdminView, GlobalSetting)
 
 
 
